cde9b7fd0cf68b11ffdfe34b37b92bfdfa0d2233
Cyril David <cyx.ucron@gmail.com>
2010-07-06 01:31:04 +0800

    Implement Imagery::Faking#mode via an attr_accessor and document it properly.

--- lib/imagery/faking.rb
+++ lib/imagery/faking.rb
@@ -3,16 +3,41 @@ module Imagery
     def self.included(base)
       base.extend ClassMethods
     end
-      
+
     module ClassMethods
-      def mode=(mode)
-        @mode = mode
-      end
+      # Allows you to define the current mode. The only special value here is
+      # `:fake`. If the mode is :fake, then Imagery::Model#save and
+      # Imagery::Model#delete will not actually run.
+      #
+      # @example
+      #
+      #   Photo = Class.new(Struct.new(:id))
+      #
+      #   i = Imagery.new(Photo.new(1001))
+      #   i.root = '/tmp'
+      #
+      #   Imagery::Model.faked {
+      #     assert i.save(File.open('/path/to/image.png'))
+      #   }
+      #
+      # @see Imagery::Test
+      #
+      attr_accessor :mode
 
-      def mode
-        @mode
-      end
-
+      # Switches the current mode to :fake.
+      #
+      # @example
+      #   Imagery::Model.mode == nil
+      #   # => true
+      #
+      #   Imagery::Model.faked {
+      #     Imagery::Model.mode == :fake
+      #     # => true
+      #   }
+      #
+      #   Imagery::Model.mode = nil
+      #   # => true
+      #
       def faked
         @omode, @mode = @mode, :fake
         yield
@@ -20,6 +45,24 @@ module Imagery
         @mode = @omode
       end
 
+      # Switches the current mode to nil. Useful for forcing real saves
+      # in your test.
+      #
+      # You should do this at least once in your project just to know
+      # that all your Imagery::Model#save and Imagery::Model#delete
+      # operations actually work.
+      #
+      # @example
+      #   Imagery::Model.mode = :fake
+      #
+      #   Imagery::Model.faked {
+      #     Imagery::Model.mode == nil
+      #     # => true
+      #   }
+      #
+      #   Imagery::Model.mode = :fake
+      #   # => true
+      #
       def real
         @omode, @mode = @mode, nil
         yield
@@ -28,12 +71,16 @@ module Imagery
       end
     end
 
+    # Implement the stubbed version of save and skips actual operation
+    # if Imagery::Model.mode == :fake
     def save(io)
       return true if self.class.mode == :fake
 
       super
     end
 
+    # Implement the stubbed version of save and skips actual operation
+    # if Imagery::Model.mode == :fake
     def delete
       return true if self.class.mode == :fake
 
