5ac0ba91c7fc56e77c55903cd2500ec5dc207aea
Cyril David <cyx.ucron@gmail.com>
2010-07-06 01:31:20 +0800

    Implement Imagery::Missing#existing via an attr_accessor and document it properly.

--- lib/imagery/missing.rb
+++ lib/imagery/missing.rb
@@ -1,12 +1,23 @@
 module Imagery
   module Missing
-    def existing=(existing)
-      @existing = existing
-    end
-
-    def existing
-      @existing
-    end
+    # Allows you to set the current filename of your photo.
+    # Much of Imagery::Missing is hinged on this.
+    #
+    # @example
+    #
+    #   Photo = Class.new(Struct.new(:id))
+    #   i = Imagery::Model.new(Photo.new(1001))
+    #   i.extend Imagery::Missing
+    #   i.url == "/photo/1001/original.png"
+    #   # => true
+    #
+    #   i.existing = "Filename"
+    #   i.url == "/missing/photo/original.png"
+    #   # => true
+    #
+    # @see Imagery::Missing#url
+    #
+    attr_accessor :existing
 
     def url(size = self.default_size)
       if existing.to_s.empty?
